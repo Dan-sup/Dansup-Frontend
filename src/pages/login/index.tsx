b45b4c29b9edfdf72a4cf42856259ad95f9a7e3d
Yoojin <yjshin1003@naver.com>
2023-07-17 03:03:38 +0900

    feat: LoginPage UI(미완)

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,14 @@
+import styles from '../../styles/LoginPage.module.css';
+import typoStyles from '../../styles/typography.module.css';
+import KakaoLogo from '../../../public/icons/kakao-logo.svg';
+
 export default function LoginPage() {
-  return <>LoginPage</>;
+  return (
+    <div className={styles.container}>
+      <button className={`${styles.loginBtn} ${typoStyles.body1_SemiBold}`}>
+        {/*<KakaoLogo />*/}
+        <div>카카오톡으로 시작하기</div>
+      </button>
+    </div>
+  );
 }
