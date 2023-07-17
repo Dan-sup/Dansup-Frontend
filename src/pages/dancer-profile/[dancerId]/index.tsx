2e32d9852ba7b3ebb65c13aff9c2f325a8219998
SujinRo <shtnwls1111@ewhain.net>
2023-07-18 06:03:03 +0900

    feat: 임의 data 만들어서 profile 부분 (src 부분 제외)

--- src/pages/dancer-profile/[dancerId]/index.tsx
+++ src/pages/dancer-profile/[dancerId]/index.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
-import styles from '../../../styles/UploadPage.module.css';
+import fonts from '../../../styles/typography.module.css';
+import styles from '../../../styles/Profile.module.css';
 import ClassUpload from '@/components/modal/ClassUpload';
 import PortfolioUpload from '@/components/modal/PortfolioUpload';
+import dancerData from '../../../jsons/dancerData.json';
 
 export default function DancerProfile() {
   const [isClassOpen, setIsClassOpen] = useState<boolean>(false);
   const [isPortfolioOpen, setIsPortfolioOpen] = useState<boolean>(false);
+  const dancers = dancerData.dancers;
 
   const openClassModal = () => {
     setIsClassOpen(true);
@@ -25,6 +28,35 @@ export default function DancerProfile() {
 
   return (
     <div className={styles.container}>
+      {dancers.map(data => (
+        <div className={styles.profilePart} key={data.username}>
+          <div className={styles.backVideo}></div>
+          <div className={styles.profileImg}></div>
+          <div className={`${styles.genreList} ${fonts.body2_Regular}`}>
+            {data.genres.map(data => (
+              <div className={styles.genreBox} key={data.genre}>
+                <div className={styles.genre}>{data.genre}</div>
+              </div>
+            ))}
+          </div>
+          <div className={`${styles.nickname} ${fonts.head1}`}>
+            {data.nickname}
+          </div>
+          <div className={`${styles.username} ${fonts.caption1_Regular}`}>
+            {data.username}
+          </div>
+          <div className={`${styles.intro} ${fonts.body2_Regular}`}>
+            {data.intro}
+          </div>
+          <div className={`${styles.hashTagList} ${fonts.body2_Regular}`}>
+            {data.hashtags.map(data => (
+              <div className={styles.hashTagBox} key={data.hashtag}>
+                <div className={styles.hashTag}>{data.hashtag}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
       <button onClick={openClassModal} className={styles.modal}>
         내수업 올리기
       </button>
