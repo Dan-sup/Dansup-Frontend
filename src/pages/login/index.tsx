fda0c1889997b55b19f0b4e21803cec1dc2f26b8
paya17 <yjshin1003@naver.com>
2023-07-19 23:31:57 +0900

    style: LoginPage 배경 수정

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,14 +2,21 @@ import styles from '../../styles/LoginPage.module.css';
 import typoStyles from '../../styles/typography.module.css';
 import KakaoLogo from '../../../public/icons/kakao-logo.svg';
 import Image from 'next/image';
-import backgroundImg from '../../../public/imgs/background.png';
+import backgroundImg from '../../../public/imgs/background.png'; //svg로 수정 필요!
 
 export default function LoginPage() {
   const KAKAO_AUTH_URL = `https://takgyun.shop/oauth2/authorization/kakao`;
 
   return (
     <div className={styles.container}>
-      <Image src={backgroundImg} alt="img" layout="fill" objectFit="cover" />
+      <Image
+        src={backgroundImg}
+        alt="img"
+        layout="fill"
+        objectFit="cover"
+        objectPosition="center"
+      />
+
       <a href={KAKAO_AUTH_URL} className={styles.a}>
         <button className={`${styles.loginBtn} ${typoStyles.body1_SemiBold}`}>
           <KakaoLogo />
