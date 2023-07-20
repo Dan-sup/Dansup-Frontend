71838365c5e43bf2aa6bf63872b8e0f37114bbb4
paya17 <yjshin1003@naver.com>
2023-07-20 18:09:26 +0900

    style: SearchHeader 추가 스타일링

--- src/components/common/Header/SearchHeader.tsx
+++ src/components/common/Header/SearchHeader.tsx
@@ -1,5 +1,7 @@
 import BackIcon from '../../../../public/icons/back.svg';
+import SearchIcon from '../../../../public/icons/search.svg';
 import styles from '../../../styles/components/common/SearchHeader.module.css';
+import typoStyles from '../../../styles/typography.module.css';
 
 export default function SearchHeader() {
   return (
@@ -8,7 +10,12 @@ export default function SearchHeader() {
         <BackIcon />
       </div>
 
-      <div className={styles.inputBox}></div>
+      <div className={styles.inputBox}>
+        <div className={styles.searchIcon}>
+          <SearchIcon />
+        </div>
+        <input className={`${styles.input} ${typoStyles.body2_Regular}`} />
+      </div>
     </div>
   );
 }
