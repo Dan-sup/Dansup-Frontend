f976f020ee6587269b8a5279442907f67d101d26
paya17 <yjshin1003@naver.com>
2023-08-02 02:21:15 +0900

    feat: favicon,title 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,19 +5,27 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RecoilRoot } from 'recoil';
 import { CookiesProvider } from 'react-cookie';
 import Layout from '@/components/Layout';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <CookiesProvider>
-      <QueryClientProvider client={queryClient}>
-        <RecoilRoot>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </RecoilRoot>
-      </QueryClientProvider>
-    </CookiesProvider>
+    <>
+      <Head>
+        <title>Dansup</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+
+      <CookiesProvider>
+        <QueryClientProvider client={queryClient}>
+          <RecoilRoot>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </RecoilRoot>
+        </QueryClientProvider>
+      </CookiesProvider>
+    </>
   );
 }
