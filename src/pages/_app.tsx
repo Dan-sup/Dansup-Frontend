62102472ed5ce3c76608b54b67c6ee68c5db2af5
paya17 <yjshin1003@naver.com>
2023-08-01 22:08:20 +0900

    feat: 새로고침시 토큰 재발급 기능 구현

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RecoilRoot } from 'recoil';
 import { CookiesProvider } from 'react-cookie';
+import Layout from '@/components/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
@@ -12,7 +13,9 @@ export default function App({ Component, pageProps }: AppProps) {
     <CookiesProvider>
       <QueryClientProvider client={queryClient}>
         <RecoilRoot>
-          <Component {...pageProps} />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
         </RecoilRoot>
       </QueryClientProvider>
     </CookiesProvider>
