e7a2dbc5e77d53c98536ec6792cd54d643a34079
SujinRo <shtnwls1111@ewhain.net>
2023-10-03 15:55:55 +0900

    feat: closeclass recoil

--- src/store/class.ts
+++ src/store/class.ts
@@ -22,6 +22,11 @@ export const bothFilteredClassListState = atom({
   //effects_UNSTABLE: [persistAtom],
 });
 
+export const closeClassListState = atom({
+  key: 'closeClassListState',
+  default: [],
+});
+
 /*
 페이지 리프레쉬해도 값 유지되도록 전역상태가 localstorage에 저장되게 하는 'recoil-persist'는 나중에!
 */
