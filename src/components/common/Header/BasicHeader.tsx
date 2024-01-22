41ab2dff249be907e06c7a13d7a964c62c48a070
sujinRo <shtnwls1111@ewhain.net>
2024-01-23 04:04:00 +0900

    feat: basicHeader에 프로필 추가

--- src/components/common/Header/BasicHeader.tsx
+++ src/components/common/Header/BasicHeader.tsx
@@ -1,10 +1,15 @@
 import BackIcon from '../../../../public/icons/back.svg';
 import DansupLogo from '../../../../public/icons/dansup-logo.svg';
 import styles from '../../../styles/components/common/BasicHeader.module.css';
+import ProfileImg from '../../../../public/icons/header-blank-profile.svg';
 import { useRecoilValue } from 'recoil';
 import { userState } from '@/store/user';
 import { useRouter } from 'next/router';
 import TopBar from '../TopBar';
+import { useMutation } from '@tanstack/react-query';
+import { getMyInfo } from '@/apis/my';
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
 
 interface BasicHeaderProps {
   type?: string;
@@ -12,6 +17,10 @@ interface BasicHeaderProps {
 
 export default function BasicHeader({ type }: BasicHeaderProps) {
   const router = useRouter();
+  const user = useRecoilValue(userState);
+  const accessToken = user.accessToken;
+
+  const [profileImg, setProfileImg] = useState<string>('');
 
   const onclickBack = () => {
     if (router.pathname === '/register') {
@@ -20,6 +29,24 @@ export default function BasicHeader({ type }: BasicHeaderProps) {
       router.back();
     }
   };
+
+  const getMyInfoMutation = useMutation(getMyInfo, {
+    onSuccess: data => {
+      //console.log(data);
+      //console.log(data.data.profileImageUrl);
+      setProfileImg(data.data.profileImageUrl);
+      //console.log(profileImg);
+    },
+    onError: error => {
+      //console.log(error);
+    },
+  });
+
+  useEffect(() => {
+    if (accessToken !== '') {
+      getMyInfoMutation.mutate(accessToken);
+    }
+  }, [accessToken, profileImg]);
 
   return (
     <TopBar color="black">
@@ -31,6 +58,19 @@ export default function BasicHeader({ type }: BasicHeaderProps) {
       {type !== 'register' && (
         <div className={styles.btn} onClick={() => router.push('/')}>
           <DansupLogo />
+        </div>
+      )}
+      {type !== 'register' && type !== 'home' && (
+        <div>
+          {accessToken === '' || profileImg === '' ? (
+            <Link href={accessToken === '' ? '/login' : '/my'}>
+              <ProfileImg className={styles.blankImg} />
+            </Link>
+          ) : (
+            <Link href={'/my'}>
+              <img src={profileImg} className={styles.profileImg} />
+            </Link>
+          )}
         </div>
       )}
     </TopBar>
