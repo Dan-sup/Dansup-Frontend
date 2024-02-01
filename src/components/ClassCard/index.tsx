1bb63b85991723ef696159dda57a51966b669e27
paya17 <yjshin1003@naver.com>
2024-02-01 21:05:45 +0900

    style: ClassCard 디자인 수정

--- src/components/ClassCard/index.tsx
+++ src/components/ClassCard/index.tsx
@@ -16,39 +16,22 @@ export default function ClassCard({ classInfo }: ClassCardProps) {
   const router = useRouter();
 
   return (
-    <div className={styles.container}>
-      <div className={styles.titleBox}>
-        <div className={styles.dancerBox}>
-          {!classInfo.userProfileImage ? (
-            <AvatarIcon className={styles.profileImg} />
-          ) : (
-            <img
-              src={classInfo.userProfileImage}
-              className={styles.profileImg}
-            />
-          )}
-
-          <div
-            className={`${styles.dancerName} ${typoStyles.caption1_SemiBold}`}
-          >
-            {classInfo.userNickname}
-          </div>
+    <>
+      <div className={styles.container}>
+        <div className={styles.scrapBtnBox}>
+          <ScrapBtn />
         </div>
 
-        <ScrapBtn />
-      </div>
+        <ReactPlayer
+          url={classInfo?.videoUrl}
+          muted
+          playing={false}
+          //className={styles.reactPlayer}
+          width="100%"
+          height="100%"
+          onClick={() => router.push(`class/${classInfo.danceClassId}`)}
+        />
 
-      <ReactPlayer
-        url={classInfo?.videoUrl}
-        muted
-        playing={false}
-        className={styles.video}
-        width="100%"
-        height={271}
-        onClick={() => router.push(`class/${classInfo.danceClassId}`)}
-      />
-
-      <div className={`${styles.InfoBox} ${typoStyles.body2_Regular}`}>
         <div className={styles.genreBox}>
           {classInfo.genres.map((item: any, idx: any) => (
             <>
@@ -57,19 +40,45 @@ export default function ClassCard({ classInfo }: ClassCardProps) {
             </>
           ))}
         </div>
+      </div>
 
-        <div
-          className={`${styles.classTitle} ${typoStyles.head1}`}
-          onClick={() => router.push(`class/${classInfo.danceClassId}`)}
-        >
-          {classInfo.title}
-        </div>
+      <div className={styles.InfoBox}>
+        <>
+          {!classInfo.userProfileImage ? (
+            <AvatarIcon className={styles.profileImg} />
+          ) : (
+            <img
+              src={classInfo.userProfileImage}
+              className={styles.profileImg}
+            />
+          )}
+        </>
 
-        <div className={styles.bottomLineBox}>
-          <Location classInfo={classInfo} />
-          <Date classInfo={classInfo} />
+        <div className={styles.rigthInfoBox}>
+          <div
+            className={`${styles.classTitle} ${typoStyles.body1_Regular}`}
+            onClick={() => router.push(`class/${classInfo.danceClassId}`)}
+          >
+            {classInfo.title}
+          </div>
+
+          <div className={styles.bottomLineBox}>
+            <div
+              className={`${styles.dancerName} ${typoStyles.caption1_Regular}`}
+            >
+              {classInfo.userNickname}
+            </div>
+
+            <div className={styles.bottomInfoDivider} />
+
+            <Location classInfo={classInfo} />
+
+            <div className={styles.bottomInfoDivider} />
+
+            <Date classInfo={classInfo} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
