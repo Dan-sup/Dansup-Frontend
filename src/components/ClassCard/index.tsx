a9d049feaf8a37679c755ee540522689d538985e
paya17 <yjshin1003@naver.com>
2024-06-26 20:04:16 +0900

    fix: ClassCard,DancerCard 라우팅 코드 문제 해결

--- src/components/ClassCard/index.tsx
+++ src/components/ClassCard/index.tsx
@@ -7,6 +7,7 @@ import ScrapBtn from '../common/ScrapBtn';
 import Location from '../common/Location';
 import Date from '../common/Date';
 import GenreChip from '../common/GenreChip';
+import Link from 'next/link';
 
 interface ClassCardProps {
   classInfo: any;
@@ -24,15 +25,22 @@ export default function ClassCard({ classInfo }: ClassCardProps) {
         </div>
         */}
 
-        <ReactPlayer
-          url={classInfo?.videoUrl}
-          muted
-          playing={false}
-          //className={styles.reactPlayer}
-          width="100%"
-          height="100%"
-          onClick={() => router.push(`class/${classInfo.danceClassId}`)}
-        />
+        <Link
+          href={{
+            pathname: `/class/[classId]`,
+            query: { classId: classInfo?.danceClassId },
+          }}
+        >
+          <ReactPlayer
+            url={classInfo?.videoUrl}
+            muted
+            playing={false}
+            //className={styles.reactPlayer}
+            width="100%"
+            height="100%"
+            //onClick={() => router.push(`/class/${classInfo.danceClassId}`)}
+          />
+        </Link>
 
         <div className={styles.genreBox}>
           {classInfo.genres.map((item: any, idx: any) => (
@@ -45,7 +53,12 @@ export default function ClassCard({ classInfo }: ClassCardProps) {
       </div>
 
       <div className={styles.InfoBox}>
-        <>
+        <Link
+          href={{
+            pathname: `/dancer-profile/[dancerId]`,
+            query: { dancerId: classInfo?.profileId },
+          }}
+        >
           {!classInfo.userProfileImage ? (
             <AvatarIcon className={styles.profileImg} />
           ) : (
@@ -54,15 +67,22 @@ export default function ClassCard({ classInfo }: ClassCardProps) {
               className={styles.profileImg}
             />
           )}
-        </>
+        </Link>
 
         <div className={styles.rigthInfoBox}>
-          <div
-            className={`${styles.classTitle} ${typoStyles.body1_Regular}`}
-            onClick={() => router.push(`class/${classInfo.danceClassId}`)}
+          <Link
+            href={{
+              pathname: `/class/[classId]`,
+              query: { classId: classInfo?.danceClassId },
+            }}
           >
-            {classInfo.title}
-          </div>
+            <div
+              className={`${styles.classTitle} ${typoStyles.body1_Regular}`}
+              //onClick={() => router.push(`/class/${classInfo.danceClassId}`)}
+            >
+              {classInfo.title}
+            </div>
+          </Link>
 
           <div className={styles.bottomLineBox}>
             <div
